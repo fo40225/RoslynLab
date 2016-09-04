9044b1e2f99715e87323754bdbbed3beac15ad12
fo40225 <fo40225@hotmail.com>
2016-09-04 18:15:19 +0800

    Creating a compilation https://github.com/dotnet/roslyn/wiki/Getting-Started-C%23-Semantic-Analysis#example---creating-a-compilation

---
+++ SemanticsCS/Program.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace SemanticsCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SyntaxTree tree = CSharpSyntaxTree.ParseText(
+@"using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(""Hello, World!"");
+        }
+    }
+}");
+
+            var root = (CompilationUnitSyntax)tree.GetRoot();
+            var compilation = CSharpCompilation.Create("HelloWorld")
+                                               .AddReferences(
+                                                    MetadataReference.CreateFromFile(
+                                                        typeof(object).Assembly.Location))
+                                               .AddSyntaxTrees(tree);
+        }
+    }
+}
