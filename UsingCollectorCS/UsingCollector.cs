e0e3b411b612ef66403a97b99f74fa700f3aff8c
fo40225 <fo40225@hotmail.com>
2016-09-04 17:49:06 +0800

    Implementing a SyntaxWalker https://github.com/dotnet/roslyn/wiki/Getting-Started-C%23-Syntax-Analysis#example---implementing-a-syntaxwalker

---
+++ UsingCollectorCS/UsingCollector.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace UsingCollectorCS
+{
+    public class UsingCollector : CSharpSyntaxWalker
+    {
+        public readonly List<UsingDirectiveSyntax> Usings = new List<UsingDirectiveSyntax>();
+
+        public override void VisitUsingDirective(UsingDirectiveSyntax node)
+        {
+            if (node.Name.ToString() != "System" && !node.Name.ToString().StartsWith("System."))
+            {
+                this.Usings.Add(node);
+            }
+        }
+    }
+}
